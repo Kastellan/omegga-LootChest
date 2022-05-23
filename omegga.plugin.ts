813096097f25010ce98d7d37d18b13c3a76631b2
Kastellan <m.moeller500@gmail.com>
2022-05-22 20:11:02 -0400

    Add files via upload

--- omegga.plugin.ts
+++ omegga.plugin.ts
@@ -34,7 +34,45 @@ export default class Plugin implements OmeggaPlugin<Config, Storage> {
 		Omegga.writeln(`Bricks.ClearRegion ${position[0]} ${position[1]} ${position[2]} 10 10 10`);
 		
 		//save the location of this box
+		
+		
 		//load item spawn brick
+		const publicUser = {
+            id: 'ffffffff-ffff-ffff-ffff-ffffffffffff',
+            name: 'Generator',
+        };
+		const save: WriteSaveObject = {
+			author: {
+				id: publicUser.id,
+                name: 'TypeScript',
+            },
+            description: 'Noise Terrain',
+            map: 'brs-js example',
+            materials: [
+                'BMC_Plastic',
+                'BMC_Metallic',
+                'BMC_Glow',
+                'BMC_Glass',
+                'BMC_Hologram',
+                'BMC_Ghost',
+                'BMC_Ghost_Fail'
+            ],
+            brick_owners: [publicUser],
+            bricks: [{
+                size: [10, 10, 2],
+                color: [63, 63, 63],
+                position: [x,y,z],
+			    components: {
+					BCD_ItemSpawn: {
+						PickupClass: 'BP_ItemPickup_AssaultRifle',
+						PickupRespawnTime: 0,
+						PickupScale: 0.8,
+					}
+			    }              
+			}]
+		};
+		let inputData = {offX: position[0], offY: position[1], offZ: position[3], quiet: true, correctPalette: true, correctCustom: false};
+        Omegga.loadSaveData(save,inputData);
       });
 
     return {};
@@ -52,7 +90,7 @@ export default class Plugin implements OmeggaPlugin<Config, Storage> {
   }
   
   async pluginEvent(event: string, from: string, ...args: any[]) {
-    console.log(event, from, args)
+    //console.log(event, from, args)
     if (event === 'roundend') {
       const [{ name }] = args;
       this.omegga.broadcast(`${name} has ended.`)
@@ -61,6 +99,7 @@ export default class Plugin implements OmeggaPlugin<Config, Storage> {
     if (event === 'roundchange') {
       const [{ name }] = args;
       this.omegga.broadcast(`${name} has reset.`);
+	  //change all the opened boxes back to closed boxes
     }
   }
 }
