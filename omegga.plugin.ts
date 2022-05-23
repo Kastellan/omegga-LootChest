f152fc2ccbb6bcf34e711782de398772f0106aae
Kastellan <m.moeller500@gmail.com>
2022-05-22 23:15:00 -0400

    Add files via upload

--- omegga.plugin.ts
+++ omegga.plugin.ts
@@ -49,7 +49,7 @@ const lootBrick: WriteSaveObject = {
 				bMatchBrickShape: false,
 			    Brightness: 80,
 			    Radius: 5,
-			    //Color: [100,100,100],
+			    Color: [0,0,0],
 			    bUseBrickColor: true,
 			    bCastShadows: false
 			}
